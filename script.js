65cd32ccae7a38478387043635846f349e8d9969
t04772478 <t04772478@gmail.com>
2026-01-21 21:45:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -307,14 +307,17 @@ function render() {
   scoreEl.textContent = score;
   energyEl.textContent = energy;
 
-   /* === ENERGY POP EFFECT === */
-if (energy < lastEnergy) {
+  /* === ENERGY ANIMATION (FAQAT 10 DAN KAM) === */
+if (energy < lastEnergy && energy <= 10) {
   energyEl.classList.remove("pop");
   void energyEl.offsetWidth; // animatsiyani qayta yoqadi
   energyEl.classList.add("pop");
+} else {
+  energyEl.classList.remove("pop");
 }
 
 lastEnergy = energy;
+
 
    /* === SCORE EFFECT === */
 if (score > lastScore) {
